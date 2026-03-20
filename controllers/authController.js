99f3b13d6bd848206465ee3a263b3b49c43fb4ee
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-03-20 10:19:52 +0530

    Signup & login controller

---
+++ controllers/authController.js
@@ -0,0 +1,50 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const token = require("../utils/generateToken.js");
+const generateToken = require("../utils/generateToken.js");
+
+const signup = async(req,res)=>{
+    try{
+        const {userName,email,password} = req.body;
+        const userExit = await User.findOne({email});
+        if(userExit){
+            return res.status(400).json({messgae:"User already Exits"});
+        }
+        const hashedpwd = await bcrypt.hash(password,10);
+        const user = await User.create({
+            userName,
+            email,
+            password:hashedpwd
+        })
+        res.status(201).json({
+            _id:user.id,
+            userName:user.userName,
+            email:user.email,
+            password:user.password,
+        });
+        
+        
+    }catch(error){
+        res.status(500).json({messgae:error.messgae});
+    }
+};
+const login = async(req,res)=>{
+    try{
+        const {userName,email,password} = req.body;
+        const user = await User.findOne({email});
+        if(user && (await bcrypt.compare(password,user.password))){
+            res.json({
+                _id:user.id,
+                userName:user.userName,
+                email:user.email,
+                token:generateToken(user._id)
+            });
+        }else{
+            res.status(401).json({messgae:"Invalid Credentials!!"});
+        }
+    }catch(err){
+        res.status(500).json({messgae:err.messgae});
+    }
+}
+
+module.exports = {signup,login};
